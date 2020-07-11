a63c929feafe06a7e99e794a777b4e25d2187ea4
soleti <roberto@lbl.gov>
2020-07-10 17:16:37 -0700

    setuptools file

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+VER = "0.0.1"
+
+setuptools.setup(
+    name="larndsim",
+    version=VER,
+    author="DUNE collaboration",
+    author_email="roberto@lbl.gov",
+    description="Simulation framework for the DUNE LArND",
+    url="https://github.com/soleti/larnd-sim",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy >=1.18", "scipy >= 1.4.0"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: by End-User Class :: Developers",
+        "Operating System :: Grouping and Descriptive Categories :: OS Independent (Written in an interpreted language)",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    python_requires='>=3.6',
+)
