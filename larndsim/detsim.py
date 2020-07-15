bb271ff88dec3217df6460ca311a5132e1d989e8
Zoya Vallari <zoya.vallari@gmail.com>
2020-07-15 12:04:16 -0700

    modularize drift and quenching stages

--- larndsim/detsim.py
+++ larndsim/detsim.py
@@ -14,7 +14,8 @@ import skimage.draw
 import numba as nb
 
 import consts
-
+import drifting
+import quenching
 #from . consts import TPC_PARAMS, PHYSICAL_PARAMS
 
 def sigmoid(t, t0, t_rise=1):
@@ -68,53 +69,6 @@ class TrackCharge:
         return integral*expo*self.factor
 
 
-class Quenching:
-    '''
-    Module to implement the quenching of the electrons
-    in the TPC.
-    '''
-    @nb.njit
-    def Quench(dEdx, dE, NElectrons):
-        '''    
-        CPU Quenching Kernel function
-        '''       
-        for index in range(dE.shape[0]):    
-            recomb = np.log(consts.alpha + consts.beta * dEdx[index] \
-                 / (consts.beta * dEdx[index]))
-            if(recomb <= 0 or np.isnan(recomb)):
-                recomb = 0
-        
-            NElectrons[index]  = recomb * dE[index] * consts.MeVToElectrons
-
-
-class Drifting:
-    '''
-    Module to implement the propagation of the
-    electrons towards the anode.
-    '''
-    @nb.njit
-    def Drift(NElectrons, z, zStart, zEnd, t, tStart, tEnd, longDiff, tranDiff):
-        '''    
-        CPU Drift function
-        '''       
-        zAnode = consts.tpcZStart
-
-        for index in range(NElectrons.shape[0]): 
-            driftDistance = np.abs(z[index] - zAnode)
-            driftStart    = np.abs(zStart[index] - zAnode)
-            driftEnd      = np.abs(zEnd[index] - zAnode)
-
-            driftTime     = driftDistance / consts.vdrift
-            z[index]      = zAnode
-
-            lifetime          = np.exp(-driftTime / consts.lifetime)
-            NElectrons[index] = NElectrons[index] * lifetime
-
-            longDiff[index] = np.sqrt(driftTime) * consts.longDiff
-            tranDiff[index] = np.sqrt(driftTime) * consts.tranDiff
-            t[index]       += driftTime + tranDiff[index]/ consts.vdrift
-            tStart[index]  += (driftStart + tranDiff[index])/ consts.vdrift
-            tEnd[index]    += (driftEnd + tranDiff[index]) / consts.vdrift
 
     
 class PixelSignal:
