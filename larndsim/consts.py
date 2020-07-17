c6638242f7142e020454535f904a19ccbcd5981d
soleti <roberto@lbl.gov>
2020-07-16 18:14:19 -0700

    Added Birks and Box recombinations

--- larndsim/consts.py
+++ larndsim/consts.py
@@ -1,33 +1,33 @@
-'''
+"""
 Detector constants
-'''
+"""
+lArDensity = 1.38 # g/cm^3
+eField = 0.50 # kV/cm
 
-lArDensity = 1.38 #g/cm^3
-eField      = 0.50 #V/sm
-
-'''
+"""
 Unit Conversions
-'''
-
-GeVToMeV       = 1e3 #MeV
+"""
+GeVToMeV = 1e3 # MeV
 MeVToElectrons = 4.237e+04
-msTous         = 10e3 # us   
+msTous = 10e3 # us
 
-'''
+"""
 PHYSICAL_PARAMS
-'''
+"""
 MeVToElectrons = 4.237e+04
-alpha          = 0.847
-beta           = 0.2061
+alpha = 0.847
+beta = 0.2061
+Ab = 0.800
+kb = 0.0486 # g/cm2/MeV Amoruso, et al NIM A 523 (2004) 275
 
-'''
+"""
 TPC_PARAMS
-'''
-vdrift =  0.153812 # u.cm / u.us,
-lifetime =  10e3 # u.us,
-tpcBorders = ((-150, 150), (-150, 150), (-150, 150)) # u.cm,
-tpcZStart = -150
-timeInterval =  (0, 3000)
-longDiff =  6.2e-6 # u.cm * u.cm / u.us,
-tranDiff =  16.3e-6 # u.cm
+"""
+vdrift = 0.153812 # cm / us,
+lifetime = 10e3 # us,
+tpcBorders = ((-150, 150), (-150, 150), (-150, 150)) # cm,
+tpcZStart = -150 # cm
+timeInterval = (0, 3000) # us
+longDiff = 6.2e-6 # cm * cm / us,
+tranDiff = 16.3e-6 # cm
 
