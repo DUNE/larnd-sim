fixed minor bug in charge calculation

larndsim/TPC.py
@@ -28,7 +28,7 @@ t_length = t_end - t_start
 
 x_sampling = x_length/n_pixels/4
 y_sampling = y_length/n_pixels/4
-t_sampling = 1
+t_sampling = 0.5
 z_sampling = t_sampling * vdrift
 
 anode_x = np.linspace(x_start, x_end, int(x_length/x_sampling))

larndsim/diffused_charge.py
@@ -63,12 +63,12 @@ def getChargeWeights(x,y,z,a,factor, start, sigmas, direction, Deltar):
                      y + TPC.y_pixel_size * 2,
                     10)
     xv, yv, zv = np.meshgrid(xx, yy, z)
-    weights = rho(xv, yv, zv, a, start,sigmas, direction, Deltar).ravel()
+    weights = rho(xv, yv, zv, a, factor, start, sigmas, direction, Deltar).ravel()
 
     return weights
 
 
-def rho(x, y, z, a, start, sigmas, direction, Deltar):
+def rho(x, y, z, a, factor, start, sigmas, direction, Deltar):
 
     position = np.array([x,y,z])
     _b = -((position.T - start) / sigmas**2 * direction).T
@@ -84,7 +84,7 @@ def rho(x, y, z, a, start, sigmas, direction, Deltar):
                 / sqrt_a_2)
     
 
-    return integral*expo
+    return factor*integral*expo
 
 
 def getZEndcaps(zIntervals, zs, ze, endcap_size):