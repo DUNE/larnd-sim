dc95cb9721f73d0e31c3a2fb7f21c51d4480c6a3
Zoya Vallari <zoya.vallari@gmail.com>
2020-08-17 14:49:19 -0700

    added the x <-> z inter change of axis

--- larndsim/drifting.py
+++ larndsim/drifting.py
@@ -4,12 +4,45 @@ electrons towards the anode.
 """
 
 from math import fabs, exp, sqrt
+import numba as nb
 from numba import cuda
 from .consts import *
 
+@nb.njit(parallel=True, fastmath=True)
+def Drift(tracks, cols):
+    """
+    This function takes as input an array of track segments and calculates
+    the properties of the segments at the anode:
+    - z coordinate at the anode
+    - number of electrons taking into account electron lifetime
+    - longitudinal diffusion
+    - transverse diffusion
+    - time of arrival at the anode
+    Args:
+        tracks (:obj:`numpy.array`): array containing the tracks segment information
+        cols (:obj:`numba.typed.Dict`): Numba dictionary containing columns names for the track array
+    """
+    zAnode = tpc_borders[2][0]
+
+    for index in nb.prange(tracks.shape[0]):
+        driftDistance = fabs(tracks[index, cols["z"]] - zAnode)
+        driftStart = fabs(tracks[index, cols["z_start"]] - zAnode)
+        driftEnd = fabs(tracks[index, cols["z_end"]] - zAnode)
+
+        driftTime = driftDistance / vdrift
+        tracks[index, cols["z"]] = zAnode
+
+        lifetime_red = exp(-driftTime / lifetime)
+        tracks[index, cols["NElectrons"]] *= lifetime_red
+
+        tracks[index, cols["longDiff"]] = sqrt(driftTime) * longDiff
+        tracks[index, cols["tranDiff"]] = sqrt(driftTime) * tranDiff
+        tracks[index, cols["t"]] += driftTime + tracks[index, cols["tranDiff"]] / vdrift
+        tracks[index, cols["t_start"]] += (driftStart + tracks[index, cols["tranDiff"]]) / vdrift
+        tracks[index, cols["t_end"]] += (driftEnd + tracks[index, cols["tranDiff"]]) / vdrift
 
 @cuda.jit
-def Drift(z_start, z_end, z_mid,
+def GPU_Drift(z_start, z_end, z_mid,
           t_start, t_end, t_mid,
           nElectrons, longDiff, tranDiff):
 
--- larndsim/quenching.py
+++ larndsim/quenching.py
@@ -5,10 +5,44 @@ through the detector
 
 from math import log, isnan
 from numba import cuda
+import numba as nb
 from . import consts
 
+@nb.njit(fastmath=True)
+def Quench(tracks, cols, mode="box"):
+    """
+    This function takes as input an array of track segments and calculates
+    the number of electrons that reach the anode plane after recombination.
+    It is possible to pick among two models: Box (Baller, 2013 JINST 8 P08005) or
+    Birks (Amoruso, et al NIM A 523 (2004) 275).
+    Args:
+        tracks (:obj:`numpy.array`): array containing the tracks segment information
+        cols (:obj:`numba.typed.Dict`): Numba dictionary containing columns names for the track array
+        mode (string, optional): recombination model. Default is "box"
+    """
+    for index in nb.prange(tracks.shape[0]):
+        dedx = tracks[index, cols["dEdx"]]
+
+        recomb = 0
+
+        if mode == "box":
+            # Baller, 2013 JINST 8 P08005
+            csi = consts.beta * dedx / (consts.eField * consts.lArDensity)
+            recomb = max(0, log(consts.alpha + csi)/csi)
+        elif mode == "birks":
+            # Amoruso, et al NIM A 523 (2004) 275
+            recomb = consts.Ab / (1 + consts.kb * dedx / (consts.eField * consts.lArDensity))
+        else:
+            raise ValueError("Invalid recombination mode: must be 'box' or 'birks'")
+
+        if isnan(recomb):
+            raise RuntimeError("Invalid recombination value")
+
+        tracks[index, cols["NElectrons"]] = recomb * tracks[index, cols["dE"]] * consts.MeVToElectrons
+
+
 @cuda.jit
-def Quench(dEdx, dE, nElectrons):
+def GPU_Quench(dEdx, dE, nElectrons):
     """
     This function takes as input an array of track segments and calculates
     the number of electrons that reach the anode plane after recombination.
