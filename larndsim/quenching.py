0cf6f896bcaf3262b12437c0ff00f0644a1248ae
Zoya Vallari <zoya.vallari@gmail.com>
2020-08-17 14:00:38 -0700

    gpu version of quenching and drifting

--- larndsim/quenching.py
+++ larndsim/quenching.py
@@ -4,11 +4,11 @@ through the detector
 """
 
 from math import log, isnan
-import numba as nb
+from numba import cuda
 from . import consts
 
-@nb.njit(fastmath=True)
-def Quench(tracks, cols, mode="box"):
+@cuda.jit
+def Quench(dEdx, dE, nElectrons):
     """
     This function takes as input an array of track segments and calculates
     the number of electrons that reach the anode plane after recombination.
@@ -20,22 +20,22 @@ def Quench(tracks, cols, mode="box"):
         cols (:obj:`numba.typed.Dict`): Numba dictionary containing columns names for the track array
         mode (string, optional): recombination model. Default is "box"
     """
-    for index in nb.prange(tracks.shape[0]):
-        dedx = tracks[index, cols["dEdx"]]
+    index = cuda.grid(1)
+    if index < dEdx.shape[0]:
 
+        mode = "box"
         recomb = 0
-
         if mode == "box":
             # Baller, 2013 JINST 8 P08005
-            csi = consts.beta * dedx / (consts.eField * consts.lArDensity)
+            csi = consts.beta * dEdx[index] / (consts.eField * consts.lArDensity)
             recomb = max(0, log(consts.alpha + csi)/csi)
         elif mode == "birks":
             # Amoruso, et al NIM A 523 (2004) 275
-            recomb = consts.Ab / (1 + consts.kb * dedx / (consts.eField * consts.lArDensity))
+            recomb = consts.Ab / (1 + consts.kb * dEdx[index] / (consts.eField * consts.lArDensity))
         else:
             raise ValueError("Invalid recombination mode: must be 'box' or 'birks'")
 
         if isnan(recomb):
             raise RuntimeError("Invalid recombination value")
 
-        tracks[index, cols["NElectrons"]] = recomb * tracks[index, cols["dE"]] * consts.MeVToElectrons
+        nElectrons[index] = recomb * dE[index] * consts.MeVToElectrons
