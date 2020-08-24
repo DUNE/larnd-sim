8b8cd144d661d164342c9141b2bf7a6112870f12
soleti <roberto@lbl.gov>
2020-08-24 10:52:41 -0700

    Cosmetic changes; added docs

--- larndsim/drifting.py
+++ larndsim/drifting.py
@@ -6,7 +6,7 @@ electrons towards the anode.
 from math import fabs, exp, sqrt
 import numba as nb
 from numba import cuda
-from .consts import *
+from .consts import long_diff, tran_diff, vdrift, tpc_borders, lifetime
 from . import indeces as i
 
 @nb.njit(parallel=True, fastmath=True)
@@ -21,26 +21,27 @@ def Drift(tracks, cols):
     - time of arrival at the anode
     Args:
         tracks (:obj:`numpy.array`): array containing the tracks segment information
-        cols (:obj:`numba.typed.Dict`): Numba dictionary containing columns names for the track array
+        cols (:obj:`numba.typed.Dict`): Numba dictionary containing columns names for
+        the track array
     """
-    zAnode = tpc_borders[2][0]
+    z_anode = tpc_borders[2][0]
 
     for index in nb.prange(tracks.shape[0]):
-        driftDistance = fabs(tracks[index, cols["z"]] - zAnode)
-        driftStart = fabs(tracks[index, cols["z_start"]] - zAnode)
-        driftEnd = fabs(tracks[index, cols["z_end"]] - zAnode)
+        drift_distance = fabs(tracks[index, cols["z"]] - z_anode)
+        drift_start = fabs(tracks[index, cols["z_start"]] - z_anode)
+        drift_end = fabs(tracks[index, cols["z_end"]] - z_anode)
 
-        driftTime = driftDistance / vdrift
-        tracks[index, cols["z"]] = zAnode
+        drift_time = drift_distance / vdrift
+        tracks[index, cols["z"]] = z_anode
 
-        lifetime_red = exp(-driftTime / lifetime)
+        lifetime_red = exp(-drift_time / lifetime)
         tracks[index, cols["NElectrons"]] *= lifetime_red
 
-        tracks[index, cols["longDiff"]] = sqrt(driftTime) * long_diff
-        tracks[index, cols["tranDiff"]] = sqrt(driftTime) * tran_diff
-        tracks[index, cols["t"]] += driftTime + tracks[index, cols["tranDiff"]] / vdrift
-        tracks[index, cols["t_start"]] += (driftStart + tracks[index, cols["tranDiff"]]) / vdrift
-        tracks[index, cols["t_end"]] += (driftEnd + tracks[index, cols["tranDiff"]]) / vdrift
+        tracks[index, cols["longDiff"]] = sqrt(drift_time) * long_diff
+        tracks[index, cols["tranDiff"]] = sqrt(drift_time) * tran_diff
+        tracks[index, cols["t"]] += drift_time + tracks[index, cols["tranDiff"]] / vdrift
+        tracks[index, cols["t_start"]] += (drift_start + tracks[index, cols["tranDiff"]]) / vdrift
+        tracks[index, cols["t_end"]] += (drift_end + tracks[index, cols["tranDiff"]]) / vdrift
 
 @cuda.jit
 def GPU_Drift(tracks):
@@ -51,24 +52,24 @@ def GPU_Drift(tracks):
     Args:
         tracks (:obj:`numpy.array`): array containing the tracks segment information
     """
-    zAnode = tpc_borders[2][0]
+    z_anode = tpc_borders[2][0]
 
     itrk = cuda.grid(1)
     if itrk < tracks.shape[0]:
         track = tracks[itrk]
-        
-        driftDistance = fabs(track[i.z] - zAnode)
-        driftStart = fabs(track[i.z_start] - zAnode)
-        driftEnd = fabs(track[i.z_end] - zAnode)
 
-        driftTime = driftDistance / vdrift
-        track[i.z] = zAnode
+        drift_distance = fabs(track[i.z] - z_anode)
+        drift_start = fabs(track[i.z_start] - z_anode)
+        drift_end = fabs(track[i.z_end] - z_anode)
 
-        lifetime_red = exp(-driftTime / lifetime)
+        drift_time = drift_distance / vdrift
+        track[i.z] = z_anode
+
+        lifetime_red = exp(-drift_time / lifetime)
         track[i.n_electrons] *= lifetime_red
 
-        track[i.long_diff] = sqrt(driftTime) * long_diff
-        track[i.tran_diff] = sqrt(driftTime) * tran_diff
-        track[i.t] += driftTime + track[i.tran_diff] / vdrift
-        track[i.t_start] += (driftStart + track[i.tran_diff]) / vdrift
-        track[i.t_end] += (driftEnd + track[i.tran_diff]) / vdrift
+        track[i.long_diff] = sqrt(drift_time) * long_diff
+        track[i.tran_diff] = sqrt(drift_time) * tran_diff
+        track[i.t] += drift_time + track[i.tran_diff] / vdrift
+        track[i.t_start] += (drift_start + track[i.tran_diff]) / vdrift
+        track[i.t_end] += (drift_end + track[i.tran_diff]) / vdrift
