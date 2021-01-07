b9f407edcdb540da4f3dd9c2ba86dae331053398
Stefano Roberto Soleti <roberto@lbl.gov>
2021-01-07 10:20:32 -0800

    CLI optimization

--- cli/simulate_pixels.py
+++ cli/simulate_pixels.py
@@ -27,20 +27,16 @@ from time import time
 
 from tqdm import tqdm
 
-def run_simulation(input_filename='../examples/qc_tpc_10k.p', 
-                   output_filename='qc_tpc_cosmics.h5',
+def run_simulation(input_filename='../examples/cosmic_qc_tpc.p', 
+                   output_filename='singlecube_cosmics.h5',
                    pixel_geometry='../examples/pixel_geometry.yaml',
-                   n_tracks=1000):
+                   n_tracks=100000):
     consts.load_pixel_geometry(pixel_geometry)
     
     tracks_np = pickle.load(open(input_filename, 'rb'))
-    x_start, x_end = np.copy(tracks_np[:,i.x_start]), np.copy(tracks_np[:,i.x_end])
-    z_start, z_end = np.copy(tracks_np[:,i.z_start]), np.copy(tracks_np[:,i.z_end])
-    tracks_np[:,i.z_start], tracks_np[:,i.z_end] = x_start, x_end
-    tracks_np[:,i.x_start], tracks_np[:,i.x_end] = z_start, z_end
+    tracks_np = np.vstack(list(tracks_np.values())).T
 
     d_tracks_np = cuda.to_device(tracks_np)
-    
     TPB = 256
     BPG = ceil(tracks_np.shape[0] / TPB)
 
@@ -56,59 +52,65 @@ def run_simulation(input_filename='../examples/qc_tpc_10k.p',
     end_drifting = time()
     print(f" {end_drifting-start_drifting:.2f} s")
         
-    step = 1000
+    step = 700
     adc_tot_list = np.empty((1,fee.MAX_ADC_VALUES))
     adc_tot_ticks_list = np.empty((1,fee.MAX_ADC_VALUES))
     unique_pix_tot = np.empty((1,2))
     tot_events = 0
     d_tracks_np = d_tracks_np[:n_tracks]
+    start_looping = time()
+    
     for itrk in tqdm(range(0,d_tracks_np.shape[0],step),desc='Simulating pixels...'):
-        selected_tracks = d_tracks_np[itrk:itrk+step]
 
-        MAX_PIXELS = 50
-        MAX_ACTIVE_PIXELS = 20
-        active_pixels = np.zeros(shape=(selected_tracks.shape[0], MAX_ACTIVE_PIXELS, 2))
+        selected_tracks = d_tracks_np[itrk:itrk+step]
+        
+        max_radius = ceil(max(selected_tracks[:,i.tran_diff])*5/consts.pixel_size[0])
+        longest_pix = ceil(max(selected_tracks[:,i.dx].copy_to_host())/consts.pixel_size[0])
+
+        MAX_PIXELS = (longest_pix*4+6)*max_radius
+        MAX_ACTIVE_PIXELS = longest_pix*2
+        
+        active_pixels = cuda.device_array(shape=(selected_tracks.shape[0], MAX_ACTIVE_PIXELS, 2))
         neighboring_pixels = np.full((selected_tracks.shape[0], MAX_PIXELS, 2), -1, dtype=np.int32)
-        d_active_pixels = cuda.to_device(active_pixels)
-        n_pixels_list = np.zeros(shape=(selected_tracks.shape[0]))
+        n_pixels_list = cuda.device_array(shape=(selected_tracks.shape[0]))
         TPB = 128
         BPG = ceil(selected_tracks.shape[0] / TPB)
+
         pixels_from_track.get_pixels[BPG,TPB](selected_tracks, 
-                                              d_active_pixels, 
+                                              active_pixels, 
                                               neighboring_pixels, 
-                                              n_pixels_list)
-
-
+                                              n_pixels_list,
+                                              max_radius)
+        
         unique_eventIDs = np.unique(selected_tracks[:,i.eventID])
         event_id_map = np.zeros_like(selected_tracks[:,i.eventID],dtype=np.int32)
         for iev, evID in enumerate(selected_tracks[:,i.eventID]):
             event_id_map[iev] = np.where(evID == unique_eventIDs)[0][0]
-        d_event_id_map = cuda.to_device(event_id_map)
-
+        
         # Here we calculate the track start times and the longest signal time
         max_length = np.array([0])
         track_starts = np.empty(selected_tracks.shape[0])
-        d_track_starts = cuda.to_device(track_starts)
         TPB = 128
         BPG = ceil(selected_tracks.shape[0] / TPB)
-        detsim.time_intervals[BPG,TPB](d_track_starts, max_length,  d_event_id_map, selected_tracks)
+        detsim.time_intervals[BPG,TPB](track_starts, max_length, event_id_map, selected_tracks)
 
         # Here we calculate the induced current signals
         signals = np.zeros((selected_tracks.shape[0], 
                             neighboring_pixels.shape[1], 
                             max_length[0]), dtype=np.float32)
-        TPB = (4,4,4)
+            
+        TPB = (16,2,16)
         blockspergrid_x = ceil(signals.shape[0] / TPB[0])
         blockspergrid_y = ceil(signals.shape[1] / TPB[1])
         blockspergrid_z = ceil(signals.shape[2] / TPB[2])
         BPG = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
-        d_signals = cuda.to_device(signals)
-
-        detsim.tracks_current[BPG,TPB](d_signals, neighboring_pixels, selected_tracks)
+        detsim.tracks_current[BPG,TPB](signals, neighboring_pixels, selected_tracks)
 
         shapes = neighboring_pixels.shape
         joined = neighboring_pixels.reshape(shapes[0]*shapes[1],2)
-        unique_pix = np.unique(joined, axis=0)[1:]
+        unique_pix = np.unique(joined, axis=0)
+        unique_pix = unique_pix[(unique_pix[:,0] != -1) & (unique_pix[:,1] != -1),:]
+        
         pixel_index_map = np.full((selected_tracks.shape[0], neighboring_pixels.shape[1]), -1)
 
         for itr in range(neighboring_pixels.shape[0]):
@@ -121,29 +123,30 @@ def run_simulation(input_filename='../examples/qc_tpc_10k.p',
                         print("More pixels than maximum value")
                     pixel_index_map[itr,ipix] = index
 
-        d_pixel_index_map = cuda.to_device(pixel_index_map)
-        pixels_signals = np.zeros((len(unique_pix),len(consts.time_ticks)*len(unique_eventIDs)*3))
-        d_pixels_signals = cuda.to_device(pixels_signals)
-        TPB = (4,4,4)
-        blockspergrid_x = ceil(d_signals.shape[0] / TPB[0])
-        blockspergrid_y = ceil(d_signals.shape[1] / TPB[1])
-        blockspergrid_z = ceil(d_signals.shape[2] / TPB[2])
-        BPG = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
-        detsim.sum_pixel_signals[BPG,TPB](d_pixels_signals, 
-                                          d_signals, 
-                                          d_track_starts, 
-                                          d_pixel_index_map)
+        pixels_signals = cuda.device_array((len(unique_pix),len(consts.time_ticks)*len(unique_eventIDs)*3))
         
-        time_ticks = np.linspace(0,len(unique_eventIDs)*consts.time_interval[1]*3,d_pixels_signals.shape[1]+1)
-        integral_list = np.zeros((d_pixels_signals.shape[0], fee.MAX_ADC_VALUES))
-        adc_ticks_list = np.zeros((d_pixels_signals.shape[0], fee.MAX_ADC_VALUES))
-        TPB = 32
-        BPG = ceil(d_pixels_signals.shape[0] / TPB)
-        fee.get_adc_values[BPG,TPB](d_pixels_signals, 
+        TPB = (8,8,8)
+        blockspergrid_x = ceil(signals.shape[0] / TPB[0])
+        blockspergrid_y = ceil(signals.shape[1] / TPB[1])
+        blockspergrid_z = ceil(signals.shape[2] / TPB[2])
+        BPG = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
+        detsim.sum_pixel_signals[BPG,TPB](pixels_signals, 
+                                          signals, 
+                                          track_starts, 
+                                          pixel_index_map)
+        
+        time_ticks = np.linspace(0,len(unique_eventIDs)*consts.time_interval[1]*3,pixels_signals.shape[1]+1)
+        integral_list = np.zeros((pixels_signals.shape[0], fee.MAX_ADC_VALUES))
+        adc_ticks_list = np.zeros((pixels_signals.shape[0], fee.MAX_ADC_VALUES))
+        TPB = 128
+        BPG = ceil(pixels_signals.shape[0] / TPB)
+
+        fee.get_adc_values[BPG,TPB](pixels_signals, 
                                     time_ticks, 
                                     integral_list, 
                                     adc_ticks_list, 
                                     consts.time_interval[1]*3*tot_events)
+
         adc_list = fee.digitize(integral_list)
         adc_tot_list = np.append(adc_tot_list, adc_list,axis=0)
         adc_tot_ticks_list = np.append(adc_tot_ticks_list, adc_ticks_list,axis=0)
