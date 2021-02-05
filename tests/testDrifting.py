aedd1fb38616c4d581a1dff38a7c7ca07e3330ed
Stefano Roberto Soleti <roberto@lbl.gov>
2021-02-05 11:00:49 -0800

    test with structured array:

--- tests/testDrifting.py
+++ tests/testDrifting.py
@@ -14,25 +14,28 @@ consts.load_pixel_geometry("larndsim/pixel_layouts/layout-2.5.0.yaml")
 class TestDrifting:
 
     tracks = np.zeros((1, 29))
-    tracks[:, i.z] = np.random.uniform(consts.module_borders[0][2][0], consts.module_borders[0][2][1], 1)
-    tracks[:, i.x] = np.random.uniform(consts.module_borders[0][0][0], consts.module_borders[0][0][1], 1)
-    tracks[:, i.y] = np.random.uniform(consts.module_borders[0][1][0], consts.module_borders[0][1][1], 1)
-    tracks[:, i.n_electrons] = np.random.uniform(1e6, 1e7, 1)
+    tracks = np.core.records.fromarrays(tracks.transpose(), 
+                                        names="eventID, z_end, trackID, tran_diff, z_start, x_end, y_end, n_electrons, pdgId, x_start, y_start, t_start, dx, long_diff, pixel_plane, t_end, dEdx, dE, t, y, x, z",
+                                        formats = "i8, f8, i8, f8, f8, f8, f8, i8, i8, f8, f8, f8, f8, f8, i8, f8, f8, f8, f8, f8, f8, f8")
+    tracks["z"] = np.random.uniform(consts.module_borders[0][2][0], consts.module_borders[0][2][1], 1)
+    tracks["x"] = np.random.uniform(consts.module_borders[0][0][0], consts.module_borders[0][0][1], 1)
+    tracks["y"] = np.random.uniform(consts.module_borders[0][1][0], consts.module_borders[0][1][1], 1)
+    tracks["n_electrons"] = np.random.uniform(1e6, 1e7, 1)
 
     def test_lifetime(self):
 
         zAnode = consts.module_borders[0][2][0]
 
-        driftDistance = np.abs(self.tracks[:, i.z] - zAnode)
+        driftDistance = np.abs(self.tracks["z"] - zAnode)
         driftTime = driftDistance / consts.vdrift
 
         lifetime = np.exp(-driftTime / consts.lifetime)
 
         tracks = self.tracks
-        electronsAtAnode = tracks[:, i.n_electrons] * lifetime
+        electronsAtAnode = tracks["n_electrons"] * lifetime
 
         TPB = 128
         BPG = ceil(tracks.shape[0] / TPB)
         drifting.drift[BPG,TPB](tracks)
 
-        assert tracks[:, i.n_electrons] == pytest.approx(electronsAtAnode)
+        assert tracks["n_electrons"] == pytest.approx(electronsAtAnode)
