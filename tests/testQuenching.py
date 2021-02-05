283745a02fb7c4145f0cccd24aedcd82af70a9ae
Stefano Roberto Soleti <roberto@lbl.gov>
2021-02-05 12:22:34 -0800

    Moved to structured arrays. Should fix #21 and #22

--- tests/testQuenching.py
+++ tests/testQuenching.py
@@ -15,29 +15,38 @@ class TestQuenching:
     """
     #normal valid values
     tracks = np.zeros((100, 22))
-    tracks[:, i.dE] = np.random.uniform(0.1, 100, 100)
-    tracks[:, i.dEdx] = np.random.uniform(1, 100, 100)
+    tracks = np.core.records.fromarrays(tracks.transpose(), 
+                                        names="eventID, dEdx, x_start, dE, t_start, z_end, trackID, x_end, y_end, n_electrons, t, dx, pdgId, y, x, long_diff, z, z_start, y_start, tran_diff, t_end, pixel_plane",
+                                        formats = "i8, f8, f8, f8, f8, f8, i8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, i8")
+    tracks["dE"] = np.random.uniform(0.1, 100, 100)
+    tracks["dEdx"] = np.random.uniform(1, 100, 100)
 
     #extreme valid values
     #a track with dEdx = 0, dE was set to 1 (any non-zero value) to test the recombination factor calculation
     track_zero = np.zeros((1,22))
-    track_zero[:,i.dE] = 1
+    track_zero = np.core.records.fromarrays(track_zero.transpose(), 
+                                            names="eventID, dEdx, x_start, dE, t_start, z_end, trackID, x_end, y_end, n_electrons, t, dx, pdgId, y, x, long_diff, z, z_start, y_start, tran_diff, t_end, pixel_plane",
+                                           formats = "i8, f8, f8, f8, f8, f8, i8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, i8")
+    track_zero["dE"] = 1
 
     #a track with extremely high dEdx
     track_inf = np.zeros((1,22))
-    track_inf[:,i.dE] = 1e10
-    track_inf[:,i.dEdx] = 1e10
+    track_inf = np.core.records.fromarrays(track_inf.transpose(), 
+                                           names="eventID, dEdx, x_start, dE, t_start, z_end, trackID, x_end, y_end, n_electrons, t, dx, pdgId, y, x, long_diff, z, z_start, y_start, tran_diff, t_end, pixel_plane",
+                                           formats = "i8, f8, f8, f8, f8, f8, i8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, i8")
+    track_inf["dE"] = 1e10
+    track_inf["dEdx"] = 1e10
 
     def test_birksModel(self):
 
         tracks_birks = np.copy(self.tracks)
-        dedx = self.tracks[:, i.dEdx]
-        de = self.tracks[:, i.dE]
+        dedx = self.tracks["dEdx"]
+        de = self.tracks["dE"]
 
         TPB = 128
         BPG = ceil(tracks_birks.shape[0] / TPB)
         quenching.quench[BPG,TPB](tracks_birks, consts.birks)
-        nelectrons = tracks_birks[:, i.n_electrons]
+        nelectrons = tracks_birks["n_electrons"]
 
         recomb = consts.Ab / (1 + consts.kb * dedx / (consts.eField * consts.lArDensity))
 
@@ -46,13 +55,13 @@ class TestQuenching:
     def test_boxModel(self):
 
         tracks_box = np.copy(self.tracks)
-        dedx = self.tracks[:, i.dEdx]
-        de = self.tracks[:, i.dE]
+        dedx = self.tracks["dEdx"]
+        de = self.tracks["dE"]
 
         TPB = 128
         BPG = ceil(tracks_box.shape[0] / TPB)
         quenching.quench[BPG,TPB](tracks_box, consts.box)
-        nelectrons = tracks_box[:, i.n_electrons]
+        nelectrons = tracks_box["n_electrons"]
 
         csi = consts.beta * dedx / (consts.eField * consts.lArDensity)
         recomb = np.log(consts.alpha + csi)/csi
@@ -62,12 +71,12 @@ class TestQuenching:
     def test_birksModel_zero(self):
 
         track_birks_zero = np.copy(self.track_zero)
-        de = self.track_zero[:, i.dE]
+        de = self.track_zero["dE"]
 
         TPB = 1
         BPG = 1
         quenching.quench[BPG,TPB](track_birks_zero, consts.birks)
-        nelectrons = track_birks_zero[:, i.n_electrons]
+        nelectrons = track_birks_zero["n_electrons"]
 
         recomb = consts.Ab
 
@@ -76,12 +85,12 @@ class TestQuenching:
     def test_boxModel_zero(self):
 
         tracks_box_zero = np.copy(self.track_zero)
-        de = self.track_zero[:, i.dE]
+        de = self.track_zero["dE"]
 
         TPB = 1
         BPG = 1
         quenching.quench[BPG,TPB](tracks_box_zero, consts.box)
-        nelectrons = tracks_box_zero[:, i.n_electrons]
+        nelectrons = tracks_box_zero["n_electrons"]
 
         recomb = 0.0
 
@@ -90,12 +99,12 @@ class TestQuenching:
     def test_birksModel_inf(self):
 
         track_birks_inf = np.copy(self.track_inf)
-        de = self.track_inf[:, i.dE]
+        de = self.track_inf["dE"]
 
         TPB = 1
         BPG = 1
         quenching.quench[BPG,TPB](track_birks_inf, consts.birks)
-        nelectrons = track_birks_inf[:, i.n_electrons]
+        nelectrons = track_birks_inf["n_electrons"]
 
         recomb = nelectrons/(de * consts.MeVToElectrons)
 
@@ -104,12 +113,12 @@ class TestQuenching:
     def test_boxModel_inf(self):
 
         tracks_box_inf = np.copy(self.track_inf)
-        de = self.track_inf[:, i.dE]
+        de = self.track_inf["dE"]
 
         TPB = 1
         BPG = 1
         quenching.quench[BPG,TPB](tracks_box_inf, consts.box)
-        nelectrons = tracks_box_inf[:, i.n_electrons]
+        nelectrons = tracks_box_inf["n_electrons"]
 
         recomb = nelectrons/(de * consts.MeVToElectrons)
 
