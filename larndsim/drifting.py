2448971dec0b32434a739100719ab52dd40b28a2
Stefano Roberto Soleti <roberto@lbl.gov>
2021-02-05 12:22:34 -0800

    Moved to structured arrays. Should fix #21 and #22

--- larndsim/drifting.py
+++ larndsim/drifting.py
@@ -3,7 +3,7 @@ Module to implement the propagation of the
 electrons towards the anode.
 """
 
-from math import fabs, exp, sqrt
+from math import fabs, exp, sqrt, isclose
 from numba import cuda
 from . import consts
 from . import indeces as i
@@ -11,7 +11,7 @@ from .consts import module_borders
 
 import logging
 logging.basicConfig()
-logger = logging.getLogger('drifting')
+logger = logging.getLogger("drifting")
 logger.setLevel(logging.WARNING)
 logger.info("DRIFTING MODULE PARAMETERS")
 
@@ -38,26 +38,29 @@ def drift(tracks):
         track = tracks[itrk]
 
         for ip, plane in enumerate(module_borders):
-            if plane[0][0] < track['x'] < plane[0][1] and \
-               plane[1][0] < track['y'] < plane[1][1] and \
-               plane[2][0] < track['z'] < plane[2][1]:
+            if plane[0][0]-1e-6 <= track["x"] <= plane[0][1]+1e-6 and \
+               plane[1][0]-1e-6 <= track["y"] <= plane[1][1]+1e-6 and \
+               plane[2][0]-1e-6 <= track["z"] <= plane[2][1]+1e-6:
                 pixel_plane = ip
-                break
+            else:
+                print(track["x"],track["y"],track["z"])
+#                 break
 
-        track['pixel_plane'] = pixel_plane
-        z_anode = module_borders[pixel_plane][2][0]
-        drift_distance = fabs(track['z'] - z_anode)
-        drift_start = fabs(min(track['z_start'],track['z_end']) - z_anode)
-        drift_end = fabs(max(track['z_start'],track['z_end']) - z_anode)
+        track["pixel_plane"] = pixel_plane
+        if pixel_plane >= 0:
+            z_anode = module_borders[pixel_plane][2][0]
+            drift_distance = abs(track["z"] - z_anode)
+            drift_start = fabs(min(track["z_start"],track["z_end"]) - z_anode)
+            drift_end = fabs(max(track["z_start"],track["z_end"]) - z_anode)
 
-        drift_time = drift_distance / consts.vdrift
-        track['z'] = z_anode
+            drift_time = drift_distance / consts.vdrift
+            track["z"] = z_anode
 
-        lifetime_red = exp(-drift_time / consts.lifetime)
-        track['n_electrons'] *= lifetime_red
+            lifetime_red = exp(-drift_time / consts.lifetime)
+            track["n_electrons"] *= lifetime_red
 
-        track['long_diff'] = sqrt(drift_time * 2 * consts.long_diff)
-        track['tran_diff'] = sqrt(drift_time * 2 * consts.tran_diff)
-        track['t'] += drift_time
-        track['t_start'] += drift_start / consts.vdrift
-        track['t_end'] += drift_end / consts.vdrift
+            track["long_diff"] = sqrt(drift_time * 2 * consts.long_diff)
+            track["tran_diff"] = sqrt(drift_time * 2 * consts.tran_diff)
+            track["t"] += drift_time
+            track["t_start"] += drift_start / consts.vdrift
+            track["t_end"] += drift_end / consts.vdrift
