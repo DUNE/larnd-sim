ff950258336306e99471f79d4ebdaaed76e211bd
Daniel Margala <danielmargala@lbl.gov>
2021-02-22 16:29:13 -0800

    use device arrays in compare

--- cli/simulate_pixels.py
+++ cli/simulate_pixels.py
@@ -188,7 +188,7 @@ def run_simulation(input_filename,
         d_neighboring_pixels = cupy.array(neighboring_pixels)
         d_unique_pix = cupy.array(unique_pix)
         d_pixel_index_map = cupy.full((selected_tracks.shape[0], neighboring_pixels.shape[1]), -1)
-        compare = neighboring_pixels[..., np.newaxis, :] == unique_pix
+        compare = d_neighboring_pixels[..., np.newaxis, :] == d_unique_pix
         indices = cupy.where(cupy.logical_and(compare[..., 0], compare[..., 1]))
         d_pixel_index_map[indices[0], indices[1]] = indices[2]
         RangePop()
