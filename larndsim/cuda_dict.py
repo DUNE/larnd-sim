92709126fb67d3129f7c39c40eac3108043bb803
peter-madigan <pmadigan@berkeley.edu>
2021-10-21 16:19:01 -0700

    Fix bugs encountered in testing

--- larndsim/cuda_dict.py
+++ larndsim/cuda_dict.py
@@ -2,7 +2,7 @@ from numba import cuda
 import cupy as cp
 
 
-_EMPTY_KEY = 0xFFFFFFFF
+_EMPTY_KEY = 2**31-1
 
 
 class CudaDict(object):
@@ -33,7 +33,7 @@ class CudaDict(object):
         self.tpb = tpb
         self.bpg = bpg
         self.default = default
-        self._hashtable_keys = cp.full(1, _EMPTY_KEY, dtype=np.int32)
+        self._hashtable_keys = cp.full(1, _EMPTY_KEY, dtype=cp.int32)
         self._hashtable_values = cp.empty(1, dtype=default.dtype)
 
     def keys(self):
@@ -48,40 +48,35 @@ class CudaDict(object):
         return self.keys(), self.values()
 
     def __getitem__(self, key):
-        encoding = cp.empty(key.shape[0], dtype=np.int32)
         values = cp.empty(key.shape[0], dtype=self._hashtable_values.dtype)
         cuda_hashtable_lookup[self.tpb, self.bpg](
-            key, values, encoding, self._hashtable_keys, self._hashtable_values,
-            self.default)
+            key, values, self._hashtable_keys, self._hashtable_values, self.default)
         return values
 
     def __setitem__(self, key, value):
         if len(self) == 0:
-            self._hashtable_keys = cp.full(key.shape[0] + 1, _EMPTY_KEY, dtype=np.int32)
+            self._hashtable_keys = cp.full(key.shape[0] + 1, _EMPTY_KEY, dtype=cp.int32)
             self._hashtable_values = cp.empty(key.shape[0] + 1, dtype=value.dtype)
         else:
             raise NotImplementedError('Trying to update CudaDict, not yet supported')
-        encoding = cp.empty(key.shape[0], dtype=np.int32)
         cuda_hashtable_insert[self.tpb, self.bpg](
-            key, value, encoding, self._hashtable_keys, self._hashtable_values)
+            key, value, self._hashtable_keys, self._hashtable_values)
 
     def __delitem__(self, key):
         if len(self) == 0:
             return
-        encoding = cp.empty(key.shape[0], dtype=np.int32)
         cuda_hashtable_delete[self.tpb, self.bpg](
-            key, encoding, self._hashtable_key_arr, self._hashtable_value_arr)
+            key, self._hashtable_key_arr, self._hashtable_value_arr)
 
     def __len__(self):
         return len(self._hashtable_keys) - 1
 
-    def __contains__(self, key):
+    def contains(self, key):
         exists = cp.zeros(key.shape[0], dtype=bool)
         if len(self) == 0:
             return exists
-        encoding = cp.empty(key.shape[0], dtype=np.int32)
         cuda_hashtable_exists[self.tpb, self.bpg](
-            key, exists, encoding, self._hashtable_keys)
+            key, exists, self._hashtable_keys)
         return exists
 
     @staticmethod
@@ -111,29 +106,24 @@ class CudaDict(object):
 _HASH_CONSTANT = 257
 
 
-@cuda.jit
-def cuda_hashtable_encode(key_arr, entries, encoding_arr):
+@cuda.jit(device=True)
+def cuda_hashtable_encode(key_arr, entries):
     '''
     Encodes keys into a integer number between 0 and entries-1
     Args:
         key_arr (: obj: `numpy.ndarray`): list of keys
         entries (int): maximum integer value to return
-        encoding_arr (: obj: `numpy.ndarray`): output array
     '''
-    ikey = cuda.grid(1)
-
-    if ikey < key_arr.shape[0]:
-        encoding_arr[ikey] = ((key_arr[ikey] * _HASH_CONSTANT) % entries)
+    return ((key_arr * _HASH_CONSTANT) % entries)
 
 
 @cuda.jit
-def cuda_hashtable_insert(key_arr, value_arr, encoding_arr, hashtable_key_arr, hashtable_value_arr):
+def cuda_hashtable_insert(key_arr, value_arr, hashtable_key_arr, hashtable_value_arr):
     '''
     Inserts keys into a hashtable
     Args:
         key_arr (: obj: `numpy.ndarray`): list of keys to insert
         value_arr (: obj: `numpy.ndarray`): list of values to insert
-        encoding_arr (: obj: `numpy.ndarray`): temporary array used to store hash indices
         hashtable_key_arr (: obj: `numpy.ndarray`): list of unique keys in hashtable
         hashtable_value_arr (: obj: `numpy.ndarray`): list of values in hashtable
     '''
@@ -141,24 +131,23 @@ def cuda_hashtable_insert(key_arr, value_arr, encoding_arr, hashtable_key_arr, h
 
     if ikey < key_arr.shape[0]:
 
-        cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0], encoding_arr[ikey])
+        encoding = cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0])
         while True:
-            cuda.atomic.compare_and_swap(hashtable_key_arr[encoding_arr[ikey]], _EMPTY_KEY, key_arr[ikey])
-            if hashtable_key_arr[encoding_arr[ikey]] == key_arr[ikey]:
-                hashtable_value_arr[encoding_arr[ikey]] = value_arr[ikey]
+            cuda.atomic.compare_and_swap(hashtable_key_arr[encoding:encoding+1], _EMPTY_KEY, key_arr[ikey])
+            if hashtable_key_arr[encoding] == key_arr[ikey]:
+                hashtable_value_arr[encoding] = value_arr[ikey]
                 break
-            encoding_arr[ikey] += (encoding_arr[ikey] + 1) % hashtable_key_arr.shape[0]
+            encoding = (encoding + 1) % hashtable_key_arr.shape[0]
 
 
 @cuda.jit
-def cuda_hashtable_lookup(key_arr, value_arr, encoding_arr, hashtable_key_arr,
-                          hashtable_value_arr, default):
+def cuda_hashtable_lookup(key_arr, value_arr, hashtable_key_arr, hashtable_value_arr,
+                          default):
     '''
     Fetches values from hashtable
     Args:
         key_arr (: obj: `numpy.ndarray`): list of keys to lookup
         value_arr (: obj: `numpy.ndarray`): output array
-        encoding_arr (: obj: `numpy.ndarray`): temporary array used to store hash indices
         hashtable_key_arr (: obj: `numpy.ndarray`): list of unique keys in hashtable
         hashtable_value_arr (: obj: `numpy.ndarray`): list of values in hashtable
         default (: obj: `numpy.ndarray`): singleton array to use if key is not present
@@ -167,49 +156,47 @@ def cuda_hashtable_lookup(key_arr, value_arr, encoding_arr, hashtable_key_arr,
 
     if ikey < key_arr.shape[0]:
 
-        cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0], encoding_arr[ikey])
+        encoding = cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0])
         while True:
-            if hashtable_key_arr[encoding_arr[ikey]] == key_arr[ikey]:
-                value_arr[ikey] = hashtable_value_arr[encoding_arr[ikey]]
+            if hashtable_key_arr[encoding] == key_arr[ikey]:
+                value_arr[ikey] = hashtable_value_arr[encoding]
                 break
-            if hashtable_key_arr[encoding_arr[ikey]] == _EMPTY_KEY:
+            elif hashtable_key_arr[encoding] == _EMPTY_KEY:
                 value_arr[ikey] = default[0]
                 break
-            encoding_arr[ikey] += (encoding_arr[ikey] + 1) % hashtable_key_arr.shape[0]
+            encoding = (encoding + 1) % hashtable_key_arr.shape[0]
 
 
 @cuda.jit
-def cuda_hashtable_exists(key_arr, exists_arr, encoding_arr, hashtable_key_arr):
+def cuda_hashtable_exists(key_arr, exists_arr, hashtable_key_arr):
     '''
     Checks if a key is present in the hashtable
     Args:
         key_arr (: obj: `numpy.ndarray`): list of keys to remove
         exists_arr (: obj: `numpy.ndarray`): output array
-        encoding_arr (: obj: `numpy.ndarray`): temporary array used to store hash indices
         hashtable_key_arr (: obj: `numpy.ndarray`): list of unique keys in hashtable
     '''
     ikey = cuda.grid(1)
 
     if ikey < key_arr.shape[0]:
 
-        cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0], encoding_arr[ikey])
+        encoding = cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0])
         while True:
-            if hashtable_key_arr[encoding_arr[ikey]] == key_arr[ikey]:
+            if hashtable_key_arr[encoding] == key_arr[ikey]:
                 exists_arr[ikey] = True
                 break
-            if hashtable_key_arr[encoding_arr[ikey]] == _EMPTY_KEY:
+            if hashtable_key_arr[encoding] == _EMPTY_KEY:
                 exists_arr[ikey] = False
                 break
-            encoding_arr[ikey] += (encoding_arr[ikey] + 1) % hashtable_key_arr.shape[0]
+            encoding = (encoding + 1) % hashtable_key_arr.shape[0]
 
 
 @cuda.jit
-def cuda_hashtable_delete(key_arr, encoding_arr, hashtable_key_arr, hashtable_value_arr):
+def cuda_hashtable_delete(key_arr, hashtable_key_arr, hashtable_value_arr):
     '''
     Removes a key from the hashtable
     Args:
         key_arr (: obj: `numpy.ndarray`): list of keys to remove
-        encoding_arr (: obj: `numpy.ndarray`): temporary array used to store hash indices
         hashtable_key_arr (: obj: `numpy.ndarray`): list of unique keys in hashtable
         hashtable_value_arr (: obj: `numpy.ndarray`): list of values in hashtable
     '''
@@ -217,11 +204,11 @@ def cuda_hashtable_delete(key_arr, encoding_arr, hashtable_key_arr, hashtable_va
 
     if ikey < key_arr.shape[0]:
 
-        cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0], encoding_arr[ikey])
+        encoding = cuda_hashtable_encode(key_arr[ikey], hashtable_key_arr.shape[0])
         while True:
-            if hashtable_key_arr[encoding_arr[ikey]] == key_arr[ikey]:
-                hashtable_key_arr[encoding_arr[ikey]] == _EMPTY_KEY
+            if hashtable_key_arr[encoding] == key_arr[ikey]:
+                hashtable_key_arr[encoding] == _EMPTY_KEY
                 break
-            if hashtable_key_arr[encoding_arr[ikey]] == _EMPTY_KEY:
+            if hashtable_key_arr[encoding] == _EMPTY_KEY:
                 break
-            encoding_arr[ikey] += (encoding_arr[ikey] + 1) % hashtable_key_arr.shape[0]
+            encoding = (encoding + 1) % hashtable_key_arr.shape[0]
