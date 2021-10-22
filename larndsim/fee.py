e2928fe3fdd43d05911f492c63240aac0e52f272
peter-madigan <pmadigan@berkeley.edu>
2021-10-21 17:12:53 -0700

    Use single number for unique pixel identifier

--- larndsim/fee.py
+++ larndsim/fee.py
@@ -16,6 +16,7 @@ from glob import glob
 import os
 from tqdm import tqdm
 from . import consts
+from .pixels_from_track import pixel2id_nojit
 from .cuda_dict import CudaDict
 
 #: Maximum number of ADC values stored per pixel
@@ -96,9 +97,10 @@ def export_to_hdf5(adc_list, adc_ticks_list, unique_pix, current_fractions, trac
         ts = adc_ticks_list[itick]
         pixel_id = unique_pix[itick]
 
-        plane_id = int(pixel_id[0] // consts.n_pixels[0])
-        tile_x = int((pixel_id[0] - consts.n_pixels[0] * plane_id) // consts.n_pixels_per_tile[0])
-        tile_y = int(pixel_id[1] // consts.n_pixels_per_tile[1])
+        tile_x, tile_y, plane_id = pixel2id_nojit(pixel_id)
+        # plane_id = int(pixel_id[0] // consts.n_pixels[0])
+        # tile_x = int((pixel_id[0] - consts.n_pixels[0] * plane_id) // consts.n_pixels_per_tile[0])
+        # tile_y = int(pixel_id[1] // consts.n_pixels_per_tile[1])
         tile_id = consts.tile_map[plane_id][tile_x][tile_y]
 
         for iadc, adc in enumerate(adcs):
@@ -120,7 +122,7 @@ def export_to_hdf5(adc_list, adc_ticks_list, unique_pix, current_fractions, trac
                 p = Packet_v2()
 
                 try:
-                    chip, channel = consts.pixel_connection_dict[rotate_tile(pixel_id%[consts.n_pixels_per_tile[0],consts.n_pixels_per_tile[1]], tile_id)]
+                    chip, channel = consts.pixel_connection_dict[rotate_tile((tile_x, tile_y), tile_id)]
                 except KeyError:
                     logger.warning("Pixel ID not valid", pixel_id)
                     continue
