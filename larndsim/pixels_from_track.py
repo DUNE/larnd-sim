1f3fdb46fee1f9a7dfec8956e3c85514ff148e8c
peter-madigan <pmadigan@berkeley.edu>
2021-11-19 12:07:46 -0800

    Fix bug in pixel ids

--- larndsim/pixels_from_track.py
+++ larndsim/pixels_from_track.py
@@ -27,14 +27,10 @@ def pixel2id(pixel_x, pixel_y, pixel_plane):
     Returns:
         unique integer id
     """
-    if pixel_x < 0 or pixel_y < 0 or pixel_x > n_pixels[0] or pixel_y > n_pixels[1]:
-        return -1
     return pixel_x + n_pixels[0] * (pixel_y + n_pixels[1] * pixel_plane)
 
 
 def pixel2id_nojit(pixel_x, pixel_y, pixel_plane):
-    if pixel_x < 0 or pixel_y < 0 or pixel_x > n_pixels[0] or pixel_y > n_pixels[1]:
-        return -1
     return pixel_x + n_pixels[0] * (pixel_y + n_pixels[1] * pixel_plane)
 
 
@@ -50,15 +46,11 @@ def id2pixel(id):
         pixel_y (int): number of pixel pitches in y-dimension
         pixel_plane (int): pixel plane number
     """
-    if id < 0:
-        return -1, -1, -1
     return (id % n_pixels[0], (id // n_pixels[0]) % n_pixels[1],
             (id // (n_pixels[0] * n_pixels[1])))
 
 
 def id2pixel_nojit(id):
-    if id < 0:
-        return -1, -1, -1
     return (id % n_pixels[0], (id // n_pixels[0]) % n_pixels[1],
             (id // (n_pixels[0] * n_pixels[1])))
 
@@ -102,16 +94,16 @@ def get_pixels(tracks, active_pixels, neighboring_pixels, n_pixels_list, radius)
     if itrk < tracks.shape[0]:
         t = tracks[itrk]
         this_border = tpc_borders[int(t["pixel_plane"])]
-        start_pixel = pixel2id(
+        start_pixel = (
             (t["x_start"] - this_border[0][0]) // pixel_pitch,
             (t["y_start"] - this_border[1][0]) // pixel_pitch,
             t["pixel_plane"])
-        end_pixel = pixel2id(
+        end_pixel = (
             (t["x_end"] - this_border[0][0]) // pixel_pitch,
             (t["y_end"] - this_border[1][0]) // pixel_pitch,
             t["pixel_plane"])
 
-        get_active_pixels(start_pixel, end_pixel, active_pixels[itrk])
+        get_active_pixels(start_pixel[0], start_pixel[1], end_pixel[0], end_pixel[1], t["pixel_plane"], active_pixels[itrk])
         n_pixels_list[itrk] = get_neighboring_pixels(active_pixels[itrk],
                                                      radius,
                                                      neighboring_pixels[itrk])
@@ -168,7 +160,7 @@ def get_num_active_pixels(x0, y0, x1, y1):
     return num_pixels
 
 @cuda.jit(device=True)
-def get_active_pixels(start_id, end_id, tot_pixels):
+def get_active_pixels(start_x, start_y, end_x, end_y, plane, tot_pixels):
     """
     Converts track segement to an array of active pixels
     using Bresenham algorithm used to convert line to grid.
@@ -180,8 +172,8 @@ def get_active_pixels(start_id, end_id, tot_pixels):
             the IDs of the pixels directly below the projection of
             the segments
     """
-    x0, y0, plane_id0 = id2pixel(start_id)
-    x1, y1, _ = id2pixel(end_id)
+    x0, y0, plane_id0 = start_x, start_y, plane #id2pixel(start_id)
+    x1, y1, _ = end_x, end_y, 0 #id2pixel(end_id)
     
     dx = x1 - x0
     dy = y1 - y0
@@ -203,10 +195,8 @@ def get_active_pixels(start_id, end_id, tot_pixels):
     for x in range(dx + 1):
         x_id = x0 + x*xx + y*yx
         y_id = y0 + x*xy + y*yy
-        plane_id = x_id // n_pixels[0]
-        
             
-        if 0 <= x_id <= n_pixels[0] and 0 <= y_id <= n_pixels[1]:
+        if 0 <= x_id < n_pixels[0] and 0 <= y_id < n_pixels[1] and plane_id0 < tpc_borders.shape[0]:
             tot_pixels[x] = pixel2id(x_id, y_id, plane_id0)
         if D >= 0:
             y += 1
@@ -244,16 +234,18 @@ def get_neighboring_pixels(active_pixels, radius, neighboring_pixels):
             for y_r in range(-radius, radius+1):
                 active_x, active_y, plane_id = id2pixel(active_pixels[pix])
                 new_x, new_y = active_x + x_r, active_y + y_r
-                new_pixel = pixel2id(new_x, new_y, plane_id)
                 is_unique = True
 
-                for ipix in range(neighboring_pixels.shape[0]):
-                    if new_pixel == neighboring_pixels[ipix]:
-                        is_unique = False
-                        break
+                if 0 <= new_x < n_pixels[0] and 0 <= new_y < n_pixels[1] and plane_id < tpc_borders.shape[0]:
+                    new_pixel = pixel2id(new_x, new_y, plane_id)
+                    
+                    for ipix in range(neighboring_pixels.shape[0]):
+                        if new_pixel == neighboring_pixels[ipix]:
+                            is_unique = False
+                            break
 
-                if is_unique and 0 <= new_x < n_pixels[0] and 0 <= new_y < n_pixels[1] and plane_id < tpc_borders.shape[0]:
-                    neighboring_pixels[count] = new_pixel
-                    count += 1
+                    if is_unique:
+                        neighboring_pixels[count] = new_pixel
+                        count += 1
 
     return count
