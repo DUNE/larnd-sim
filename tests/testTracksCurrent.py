373691fb4f79882926edd87b8cbda40f047aee05
Stefano Roberto Soleti <roberto@lbl.gov>
2021-11-29 16:11:59 -0800

    fixed tests

--- tests/testTracksCurrent.py
+++ tests/testTracksCurrent.py
@@ -6,7 +6,7 @@ import pytest
 from larndsim import consts
 
 consts.load_detector_properties("larndsim/detector_properties/module0.yaml",
-                                "larndsim/pixel_layouts/multi_tile_layout-2.1.16.yaml")
+                                "larndsim/pixel_layouts/multi_tile_layout-2.3.16.yaml")
 
 from larndsim import detsim
 from larndsim import drifting, quenching, pixels_from_track
@@ -14,7 +14,10 @@ from larndsim import drifting, quenching, pixels_from_track
 from math import ceil
 
 class TestTrackCurrent:
-    tracks = np.zeros((10, 29))
+    tracks = np.zeros((10, 23))
+    tracks = np.core.records.fromarrays(tracks.transpose(), 
+                                        names="eventID, dEdx, x_start, dE, t_start, z_end, trackID, x_end, y_end, n_electrons, n_photons, t, dx, pdgId, y, x, long_diff, z, z_start, y_start, tran_diff, t_end, pixel_plane",
+                                        formats = "i8, f8, f8, f8, f8, f8, i8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, i8")
     tracks["z_start"] = np.random.uniform(consts.tpc_borders[0][2][0], consts.tpc_borders[0][2][1], 10)
     tracks["z_end"] = np.random.uniform(consts.tpc_borders[0][2][0], consts.tpc_borders[0][2][0]+2, 10)
     tracks["z"] = (tracks["z_end"]+tracks["z_start"])/2.
@@ -41,8 +44,8 @@ class TestTrackCurrent:
         MAX_PIXELS = 110
         MAX_ACTIVE_PIXELS = 50
 
-        active_pixels = np.full((tracks.shape[0], MAX_ACTIVE_PIXELS, 2), -1, dtype=np.int32)
-        neighboring_pixels = np.full((tracks.shape[0], MAX_PIXELS, 2), -1, dtype=np.int32)
+        active_pixels = np.full((tracks.shape[0], MAX_ACTIVE_PIXELS), -1, dtype=np.int32)
+        neighboring_pixels = np.full((tracks.shape[0], MAX_PIXELS), -1, dtype=np.int32)
 
         n_pixels_list = np.zeros(shape=(tracks.shape[0]))
         blockspergrid = ceil(tracks.shape[0] / threadsperblock)
@@ -54,14 +57,16 @@ class TestTrackCurrent:
         signals = np.zeros((tracks.shape[0],
                             neighboring_pixels.shape[1],
                             consts.time_ticks.shape[0]), dtype=np.float32)
-        threadsperblock = (4,4,4)
+        threadsperblock = (1,1,64)
         blockspergrid_x = ceil(signals.shape[0] / threadsperblock[0])
         blockspergrid_y = ceil(signals.shape[1] / threadsperblock[1])
         blockspergrid_z = ceil(signals.shape[2] / threadsperblock[2])
         blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
-        
+        response = np.load('larndsim/response_44.npy')
         detsim.tracks_current[blockspergrid,threadsperblock](signals,
                                                              neighboring_pixels,
-                                                             tracks)
+                                                             tracks,
+                                                             response)
+        print(np.sum(signals)*consts.t_sampling/consts.e_charge)
 
-        assert np.sum(signals)*consts.t_sampling/consts.e_charge == pytest.approx(np.sum(tracks[:,i.n_electrons]), rel=0.05)
+        assert np.sum(signals)*consts.t_sampling/consts.e_charge == pytest.approx(np.sum(tracks['n_electrons']), rel=0.05)
