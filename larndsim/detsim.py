399d9fa09261be5677f45a099ca7cd3863ed0fe4
peter-madigan <pmadigan@berkeley.edu>
2021-12-16 14:07:02 -0800

    Only perform MC calculation on track segment that overlaps with pixel

--- larndsim/detsim.py
+++ larndsim/detsim.py
@@ -16,7 +16,7 @@ from .consts import detector, physics
 from .pixels_from_track import id2pixel
 
 MAX_TRACKS_PER_PIXEL = 5
-MIN_STEP_SIZE = 0.1 # cm
+MIN_STEP_SIZE = 0.001 # cm
 MC_SAMPLE_MULTIPLIER = 1
 
 @cuda.jit
@@ -272,6 +272,45 @@ def get_closest_waveform(x, y, t, response):
 
     return 0
 
+@nb.njit
+def overlapping_segment(x, y, start, end, radius):
+    """
+    Calculates the segment of the track defined by start, end that overlaps
+    with a circle centered at x,y
+    
+    """
+    dxy = x - start[0], y - start[1]
+    v = end[0] - start[0], end[1] - start[1], end[2] - start[2]
+    l = sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+    v = v[0]/l, v[1]/l, v[2]/l
+    
+    s = (dxy[0] * v[0] + dxy[1] * v[1])/l # position of point of closest approach
+    
+    r = sqrt(dxy[0]**2 + dxy[1]**2)
+    if r > radius:
+        return start, start # no overlap
+    
+    s_plus = s + sqrt(radius**2 - r**2) / l
+    s_minus = s - sqrt(radius**2 - r**2) / l
+    
+    if s_plus > 1:
+        s_plus = 1
+    elif s_plus < 0:
+        s_plus = 0
+    if s_minus > 1:
+        s_minus = 1
+    elif s_minus < 0:
+        s_minus = 0
+        
+    new_start = (start[0] * (1 - s_minus) + end[0] * s_minus,
+                 start[1] * (1 - s_minus) + end[1] * s_minus,
+                 start[2] * (1 - s_minus) + end[2] * s_minus)
+    new_end = (end[0] * (1 - s_plus) + end[0] * s_plus,
+               end[1] * (1 - s_plus) + end[1] * s_plus,
+               end[2] * (1 - s_plus) + end[2] * s_plus)
+
+    return new_start, new_end
+
 @cuda.jit
 def tracks_current_mc(signals, pixels, tracks, response, rng_states):
     """
@@ -318,41 +357,49 @@ def tracks_current_mc(signals, pixels, tracks, response, rng_states):
 
             direction = (segment[0]/length, segment[1]/length, segment[2]/length)
             sigmas = (t["tran_diff"], t["tran_diff"], t["long_diff"])
-            step = max(min(sigmas), MIN_STEP_SIZE) # minimum resolution of 0.1mm
-            nstep = round(length/step)
+            step = MIN_STEP_SIZE
             
-            sample_q = t["n_electrons"] / (MC_SAMPLE_MULTIPLIER * nstep)
+            impact_factor = sqrt(response.shape[0]**2 + 
+                                     response.shape[1]**2) * detector.RESPONSE_BIN_SIZE
+
+            subsegment_start, subsegment_end = overlapping_segment(x_p, y_p, start, end, impact_factor)
+            subsegment = (subsegment_end[0]-subsegment_start[0], 
+                          subsegment_end[1]-subsegment_start[1], 
+                          subsegment_end[2]-subsegment_start[2])
+            subsegment_length = sqrt(subsegment[0]**2 + subsegment[1]**2 + subsegment[2]**2)
+            if subsegment_length == 0:
+                return
+                
+            nstep = round(subsegment_length / step)
+            step = subsegment_length / nstep # refine step size
             
+            charge = t["n_electrons"] * (subsegment_length/length) / (nstep*MC_SAMPLE_MULTIPLIER)
             total_current = 0
             for istep in range(nstep):
                 for _ in range(MC_SAMPLE_MULTIPLIER):
-                    x = start[0] + istep * direction[0]
-                    y = start[1] + istep * direction[1]
-                    z = start[2] + istep * direction[2]
-                    
-                    # apply diffusion
-                    x += xoroshiro128p_normal_float32(rng_states, cuda.grid(1)) * sigmas[0]
-                    y += xoroshiro128p_normal_float32(rng_states, cuda.grid(1)) * sigmas[1]
+                    x = subsegment_start[0] + step * istep * direction[0]
+                    y = subsegment_start[1] + step * istep * direction[1]
+                    z = subsegment_start[2] + step * istep * direction[2]
+                
                     z += xoroshiro128p_normal_float32(rng_states, cuda.grid(1)) * sigmas[2]
-                    
                     t0 = abs(z - TPC_BORDERS[t["pixel_plane"]][2][0]) / detector.V_DRIFT - detector.TIME_WINDOW
-
                     if not t0 < time_tick < t0 + detector.TIME_WINDOW:
                         continue
-                        
+                
+                    x += xoroshiro128p_normal_float32(rng_states, cuda.grid(1)) * sigmas[0]
+                    y += xoroshiro128p_normal_float32(rng_states, cuda.grid(1)) * sigmas[1]
                     x_dist = abs(x_p - x)
                     y_dist = abs(y_p - y)
+                
                     if x_dist > detector.RESPONSE_BIN_SIZE * response.shape[0]:
                         continue
                     if y_dist > detector.RESPONSE_BIN_SIZE * response.shape[1]:
                         continue
-                    
-                    total_current += get_closest_waveform(x_dist, y_dist, time_tick-t0, response) * sample_q * physics.E_CHARGE
+
+                    total_current += charge * get_closest_waveform(x_dist, y_dist, time_tick-t0, response) * physics.E_CHARGE
 
             signals[itrk,ipix,it] = total_current
-                    
-            
-            
+
 
 @cuda.jit
 def tracks_current(signals, pixels, tracks, response):
