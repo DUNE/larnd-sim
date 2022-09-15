cc51e63e7400d7f8eb5b89c55f51256acfd5b6fd
drinkingkazu <kterao@slac.stanford.edu>
2022-09-15 15:02:13 -0700

    a way to avoid duplicate cupy installation

--- setup.py
+++ setup.py
@@ -4,6 +4,21 @@ import setuptools
 
 VER = "0.3.1"
 
+reqs = ["numpy", "pytest", "numba==0.52", "larpix-control", "larpix-geometry", "tqdm", "fire"]
+
+try:
+    import cupy
+    msg = '''
+    ############ INFORMATION ############
+    Detected and using the installed cupy
+    Version: %s
+    Source : %s
+    #####################################\n
+    '''
+    print(msg % (str(cupy.__version__),str(cupy.__file__)))
+except ImportError:
+    reqs.append('cupy')
+
 setuptools.setup(
     name="larndsim",
     version=VER,
@@ -13,7 +28,7 @@ setuptools.setup(
     url="https://github.com/DUNE/larnd-sim",
     packages=setuptools.find_packages(),
     scripts=["cli/simulate_pixels.py", "cli/dumpTree.py"],
-    install_requires=["numpy", "pytest", "numba>=0.52", "larpix-control", "larpix-geometry", "tqdm", "fire", "cupy"],
+    install_requires=reqs,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: by End-User Class :: Developers",
