c157fc19080cd01260a769eaef5e8f362dce53ec
peter-madigan <pmadigan@berkeley.edu>
2022-10-06 13:20:40 -0700

    Improve memory locality for current calculation

--- cli/simulate_pixels.py
+++ cli/simulate_pixels.py
@@ -439,7 +439,7 @@ def run_simulation(input_filename,
             signals = cp.zeros((selected_tracks.shape[0],
                                 neighboring_pixels.shape[1],
                                 cp.asnumpy(max_length)[0]), dtype=np.float32)
-            TPB = (8,8,8)
+            TPB = (1,1,64)
             BPG_X = ceil(signals.shape[0] / TPB[0])
             BPG_Y = ceil(signals.shape[1] / TPB[1])
             BPG_Z = ceil(signals.shape[2] / TPB[2])
