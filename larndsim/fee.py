2c6976932b527e2052fbf44898a66df953462a1b
Fogarty <sfogarty@volt001.slac.stanford.edu>
2023-03-28 12:53:03 -0700

    Add gain input parameter to digitize()

--- larndsim/fee.py
+++ larndsim/fee.py
@@ -192,7 +192,7 @@ def export_to_hdf5(event_id_list,
         for iadc, adc in enumerate(adcs):
             t = ts[iadc]
 
-            if adc > digitize(0):
+            if adc > digitize(0, 0):
                 while True:
                     event = event_id_list[itick,iadc]
                     event_t0 = event_start_time_list[itick]
@@ -296,19 +296,20 @@ def export_to_hdf5(event_id_list,
     return packets, packets_mc_ds
 
 
-def digitize(integral_list):
+def digitize(integral_list, gain):
     """
     The function takes as input the integrated charge and returns the digitized
     ADC counts.
 
     Args:
         integral_list(:obj:`numpy.ndarray`): list of charge collected by each pixel
+        gain(:obj:`numpy.ndarray`): list of gain values (or float) for each pixel
 
     Returns:
         :obj:`numpy.ndarray`: list of ADC values for each pixel
     """
     xp = cp.get_array_module(integral_list)
-    adcs = xp.minimum(xp.around(xp.maximum((integral_list * GAIN + V_PEDESTAL - V_CM), 0)
+    adcs = xp.minimum(xp.around(xp.maximum((integral_list * gain + V_PEDESTAL - V_CM), 0)
                                 * ADC_COUNTS / (V_REF - V_CM)), ADC_COUNTS-1)
 
     return adcs
