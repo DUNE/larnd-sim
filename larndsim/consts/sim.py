458218edfeb5b0a9728eeeb0cf0d98f62fe2b2c2
Matt Kramer <mjkramer@users.noreply.github.com>
2023-05-04 18:12:33 -0700

    Fix indexing into event_times

--- larndsim/consts/sim.py
+++ larndsim/consts/sim.py
@@ -17,6 +17,10 @@ EVENT_SEPARATOR = 'vertexID'  # 'spillID' or 'vertexID'
 IS_SPILL_SIM = True
 SPILL_PERIOD = 1.2e6  # units = microseconds
 
+# We mod event IDs by MAX_EVENTS_PER_FILE to get zero-based IDs for indexing
+# purposes; see comments in simulate_pixels.py
+MAX_EVENTS_PER_FILE = 1000
+
 def set_simulation_properties(simprop_file):
     """
     The function loads the detector properties and
@@ -34,6 +38,7 @@ def set_simulation_properties(simprop_file):
     global EVENT_SEPARATOR
     global IS_SPILL_SIM
     global SPILL_PERIOD
+    global MAX_EVENTS_PER_FILE
     
 
     with open(simprop_file) as df:
@@ -45,4 +50,4 @@ def set_simulation_properties(simprop_file):
     EVENT_SEPARATOR = simprop['event_separator']
     IS_SPILL_SIM = simprop['is_spill_sim']
     SPILL_PERIOD = float(simprop['spill_period'])
-
+    MAX_EVENTS_PER_FILE = simprop['max_events_per_file']
