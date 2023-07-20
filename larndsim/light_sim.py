5a7d4dbe9b3b5ae3f460e7ff121aebf8024362cf
Angela Jane White <ajwhite@login35.chn>
2023-07-20 10:25:30 -0700

    added generalized digitization code

--- larndsim/light_sim.py
+++ larndsim/light_sim.py
@@ -373,10 +373,10 @@ def gen_light_detector_noise(shape, light_det_noise):
     noise = noise_spectrum * cp.exp(2j * cp.pi * cp.random.uniform(size=noise_spectrum.shape))
     if shape[1] < 2:
         # special case where inverse FFT does not exist - just generate one sample
-        noise = cp.real(noise)
+        noise = cp.round(cp.real(noise)) * 2**(16-LIGHT_NBIT)
     else:
         # invert FFT to create a noise waveform
-        noise = cp.fft.irfft(noise, axis=-1)
+        noise = cp.round(cp.fft.irfft(noise, axis=-1)) * 2**(16-LIGHT_NBIT)
 
     if noise.shape[1] < shape[1]:
         # FFT must have even samples, so append 0 if an odd number of samples is requested
