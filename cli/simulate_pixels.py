44554dccc6b6a6b1d78a848d6865a6211e162586
liviocali <livio.calivers@lhep.unibe.ch>
2023-07-21 21:57:08 +0200

    fix localSpillID for files with no event on first spill

--- cli/simulate_pixels.py
+++ cli/simulate_pixels.py
@@ -296,7 +296,7 @@ def run_simulation(input_filename,
         # The spill starts are marking the start of 
         # The space between spills will be accounted for in the
         # packet timestamps through the event_times array below
-        localSpillIDs = tracks[sim.EVENT_SEPARATOR] - tracks[0][sim.EVENT_SEPARATOR]
+        localSpillIDs = localSpillIDs = tracks[sim.EVENT_SEPARATOR] - (tracks[sim.EVENT_SEPARATOR] // sim.MAX_EVENTS_PER_FILE) * sim.MAX_EVENTS_PER_FILE
         tracks['t0_start'] = tracks['t0_start'] - localSpillIDs*sim.SPILL_PERIOD
         tracks['t0_end'] = tracks['t0_end'] - localSpillIDs*sim.SPILL_PERIOD
         tracks['t0'] = tracks['t0'] - localSpillIDs*sim.SPILL_PERIOD
