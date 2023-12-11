45a710ce550385ea7a5393130049de4fc3d3a77e
Marjolein van Nuland-Troost <fmtroost@gmail.com>
2023-12-11 17:54:18 -0600

    Update simulate_pixels.py fix cuda bug

--- cli/simulate_pixels.py
+++ cli/simulate_pixels.py
@@ -755,17 +755,14 @@ def run_simulation(input_filename,
                 for idet in range(n_idet):
                   sorted_indices[idet] = np.argsort(light_inc[:,idet]['n_photons_det'])[::-1]
 
-                # Transfer sorted_indices to the GPU
-                sorted_indices_device = cuda.to_device(sorted_indices)
                 TPB = (1,64)
                 BPG = (max(ceil(light_sample_inc.shape[0] / TPB[0]),1),
                        max(ceil(light_sample_inc.shape[1] / TPB[1]),1))
                 light_sim.sum_light_signals[BPG, TPB](
                     selected_tracks, track_light_voxel[batch_mask][itrk:itrk+sim.BATCH_SIZE], selected_track_id,
                     light_inc, op_channel, lut, light_t_start, light_sample_inc, light_sample_inc_true_track_id,
-                    light_sample_inc_true_photons, sorted_indices_device)
-                #print('Sum_light_signals photons:', photooons)
-                #print('Sum_light_signals segment/track ids:', seeeg_track_id)
+                    light_sample_inc_true_photons, sorted_indices)
+
                 RangePop()
                 if light_sample_inc_true_track_id.shape[-1] > 0 and cp.any(light_sample_inc_true_track_id[...,-1] != -1):
                     warnings.warn(f"Maximum number of true segments ({light.MAX_MC_TRUTH_IDS}) reached in backtracking info, consider increasing MAX_MC_TRUTH_IDS (larndsim/consts/light.py)")
