373a8001c6cc5245bfc589d600609425277e8c37
Marjolein van Nuland-Troost <fmtroost@gmail.com>
2023-12-11 18:05:22 -0600

    Update light_sim.py remove to_device nonsense

--- larndsim/light_sim.py
+++ larndsim/light_sim.py
@@ -85,7 +85,7 @@ def sum_light_signals(segments, segment_voxel, segment_track_id, light_inc, op_c
             
             # trying a thingy for getting the most relevant tracks first
             # Loop over the segments in the order from most to least photons
-            for itrk in cuda.to_device(sorted_indices):
+            for itrk in sorted_indices:
             #for itrk in range(segments.shape[0]):
                 if light_inc[itrk,op_channel[idet]]['n_photons_det'] > 0:
                     voxel = segment_voxel[itrk]
