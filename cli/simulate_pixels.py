72084b8e3cdb4b499882c8e9f70d7c0208873cd6
mnuland <mnuland@nikhef.nl>
2023-12-14 17:42:07 -0600

    Add print statement for debugging

--- cli/simulate_pixels.py
+++ cli/simulate_pixels.py
@@ -713,8 +713,8 @@ def run_simulation(input_filename,
                 sorted_indices = np.zeros((n_light_det, selected_tracks.shape[0]), dtype=np.int32)
 
                 for idet in range(n_light_det):
-                    print(light_inc[:,idet]['n_photons_det'])
-                    sorted_indices[idet] = np.argsort(light_inc[:,idet]['n_photons_det'])[::-1]
+                    sorted_indices[idet] = np.argsort(light_inc[:,idet]['n_photons_det'])[::-1] # get the order in which to loop over tracks
+                    print(sorted_indices[idet])
                 ### END OF TEMPORARY FIX ###
 
                 TPB = (1,64)
