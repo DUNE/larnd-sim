7586bab6a9666abda0ee29b57045366f94453768
mnuland <mnuland@nikhef.nl>
2023-12-14 14:19:08 -0600

    Increase light threshold to 5 because of memory error

--- larndsim/consts/light.py
+++ larndsim/consts/light.py
@@ -6,9 +6,9 @@ import numpy as np
 import os
 
 #: Number of true segments to track for each time tick (`MAX_MC_TRUTH_IDS=0` to disable complete truth tracking)
-MAX_MC_TRUTH_IDS = 3 #256
+MAX_MC_TRUTH_IDS = 3 # higher is better, but file size increases
 #: Threshold for propogating truth information on a given SiPM
-MC_TRUTH_THRESHOLD = 2 # 0.1 pe/us
+MC_TRUTH_THRESHOLD = 5 # was 0.1 pe/us lower is better, but memory usage increases
 ENABLE_LUT_SMEARING = False
 
 N_OP_CHANNEL = 0
