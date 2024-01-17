9865a7284958aa8c6aace6852d033e0e52eba4ba
Angela White <123964490+AWh1t3@users.noreply.github.com>
2024-01-17 02:29:25 -0600

    Update quenching.py: recombination = truth

--- larndsim/quenching.py
+++ larndsim/quenching.py
@@ -5,8 +5,8 @@ through the detector
 
 from math import log, isnan
 from numba import cuda
-import cupy as cp
-from cupy.random import choice
+#import cupy as cp
+#from cupy.random import choice
 #import random
 
 from .consts import detector, physics, light
@@ -24,14 +24,14 @@ def quench(tracks, mode, light_recomb):
         mode (int): recombination model (physics.BOX or physics.BIRKS).
     """
 
-    def random_value_from_distribution(x_vals, normalized_distribution):
-        return choice(x_vals, p=normalized_distribution/cp.sum(normalized_distribution))
+    #def random_value_from_distribution(x_vals, normalized_distribution):
+    #    return choice(x_vals, p=normalized_distribution/cp.sum(normalized_distribution))
 
-    def MIP_dEdx(mc_array):
-        bin_centers = mc_array[:,0]
-        fitted_data = mc_array[:,1]
-        random_sample = random_value_from_distribution(bin_centers, fitted_data)
-        return random_sample
+    #def MIP_dEdx(mc_array):
+    #    bin_centers = mc_array[:,0]
+    #    fitted_data = mc_array[:,1]
+    #    random_sample = random_value_from_distribution(bin_centers, fitted_data)
+    #    return random_sample
         
     itrk = cuda.grid(1)
 
@@ -46,9 +46,9 @@ def quench(tracks, mode, light_recomb):
             recomb = max(0, log(physics.BOX_ALPHA + csi)/csi)
         elif mode == physics.BIRKS:
             # Amoruso, et al NIM A 523 (2004) 275
-            moyald = MIP_dEdx(light_recomb)
-            recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * moyald / (detector.E_FIELD * detector.LAR_DENSITY))
-            #recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * dEdx / (detector.E_FIELD * detector.LAR_DENSITY))
+            #moyald = MIP_dEdx(light_recomb)
+            #recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * moyald / (detector.E_FIELD * detector.LAR_DENSITY))
+            recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * dEdx / (detector.E_FIELD * detector.LAR_DENSITY))
             #recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * 1.85 / (detector.E_FIELD * detector.LAR_DENSITY))
         else:
             raise ValueError("Invalid recombination mode: must be 'physics.BOX' or 'physics.BIRKS'")
