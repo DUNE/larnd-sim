417f8f109ce19c9fd314b40915c00841e1d3bd32
Marjolein van Nuland - Troost <mnuland@login05.chn.perlmutter.nersc.gov>
2024-02-01 02:41:16 -0800

    Use small function to get the correct shape

--- larndsim/light_sim.py
+++ larndsim/light_sim.py
@@ -656,9 +656,13 @@ def export_light_wvfm_to_hdf5(event_id, waveforms, output_filename, waveforms_tr
             ]
             for evt_idx, det_idx, tick_idx, _ in nonzero_idx
             ]
-            print(len(list_of_tuples))
+            def tuple_to_list(list_of_tuples):
+                new_list = []
+                for i in range(len(list_of_tuples)):
+                    new_list.append(tuple(list(x) for x in list_of_tuples[i]))
+                return new_list
 
-            truth_data[nonzero_idx[:, 0], nonzero_idx[:, 1]] = np.array(tuple(zip(*list_of_tuples)), dtype=truth_dtype)
+            truth_data[nonzero_idx[:, 0], nonzero_idx[:, 1]] = np.array(tuple_to_list(list_of_tuples), dtype=truth_dtype)
 
         # the final dataset will be (n_triggers, all op channels in the detector, waveform samples)
         # it would take too much memory if we hold the information until all the modules been simulated
