870b1691339c95d6c648dd3dac31801b98f6411c
Marjolein van Nuland - Troost <mnuland@login05.chn.perlmutter.nersc.gov>
2024-02-01 02:53:14 -0800

    Remove function and try yet another way

--- larndsim/light_sim.py
+++ larndsim/light_sim.py
@@ -656,13 +656,8 @@ def export_light_wvfm_to_hdf5(event_id, waveforms, output_filename, waveforms_tr
             ]
             for evt_idx, det_idx, tick_idx, _ in nonzero_idx
             ]
-            def tuple_to_list(list_of_tuples):
-                new_list = []
-                for i in range(len(list_of_tuples)):
-                    new_list.append(tuple(list(x) for x in list_of_tuples[i]))
-                return new_list
 
-            truth_data[nonzero_idx[:, 0], nonzero_idx[:, 1]] = np.array(tuple_to_list(list_of_tuples), dtype=truth_dtype)
+            truth_data[nonzero_idx[:, 0], nonzero_idx[:, 1]] = [tuple(zip(*t)) for t in zip(*list_of_tuples)]
 
         # the final dataset will be (n_triggers, all op channels in the detector, waveform samples)
         # it would take too much memory if we hold the information until all the modules been simulated
