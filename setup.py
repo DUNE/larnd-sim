05e794018c72dd90a582426f5b7470615c92ba6d
Matt Kramer <mjkramer@users.noreply.github.com>
2024-10-08 09:46:29 -0700

    Add support for CUDA 12 and pynvjitlink

--- setup.py
+++ setup.py
@@ -18,11 +18,21 @@ except ImportError:
     reqs.append('cupy')
 
 import os
-if 'SKIP_CUPY_INSTALL' in os.environ:
-    try:
-        _ = reqs.pop(reqs.index('cupy'))
-    except ValueError:
-        pass
+try:
+    cuda_dir = os.path.basename(os.environ['CUDA_HOME'])
+    cuda_ver = float(cuda_dir)
+    cuda_major_ver = int(cuda_ver)
+except:
+    cuda_ver = cuda_major_ver = -1
+
+if 'cupy' in reqs:
+    if 'SKIP_CUPY_INSTALL' in os.environ:
+        reqs.remove('cupy')
+    else:
+        if 'ALWAYS_COMPILE_CUPY' not in os.environ:
+            if cuda_major_ver in [11, 12]:
+                reqs.remove('cupy')
+                reqs.append(f'cupy-cuda{cuda_major_ver}x')
 
 import setuptools
 
@@ -54,3 +64,8 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
 )
+
+if os.getenv('LMOD_SYSTEM_NAME') == 'perlmutter':
+    # Revisit this after Perlmutter driver update (currently 525.105.17)
+    if cuda_ver >= 12.3 and 'SKIP_PYNVJITLINK_INSTALL' not in os.environ:
+        os.system('pip install --extra-index-url https://pypi.nvidia.com pynvjitlink-cu12')
