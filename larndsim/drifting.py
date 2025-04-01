fbffdc874b41d5bf1ee07d41bc9f756612bf2b06
Yifan Chen <cyifan@sdfiana007.sdf.slac.stanford.edu>
2025-04-01 12:24:56 -0700

    some minor changes on ['t'] (readout time)

--- larndsim/drifting.py
+++ larndsim/drifting.py
@@ -53,6 +53,8 @@ def drift(tracks):
             track["long_diff"] = sqrt(drift_time * 2 * detector.LONG_DIFF)
             track["tran_diff"] = sqrt(drift_time * 2 * detector.TRAN_DIFF)
 
-            track["t"] += drift_time + track["t0"]
-            track["t_start"] += min(drift_start, drift_end) / detector.V_DRIFT + track["t0"]
-            track["t_end"] += max(drift_start, drift_end) / detector.V_DRIFT + track["t0"]
+            # note t_start and t_end indicate the early and late ends in the segement drift time
+            # which does not necessarily correspond to t0_start and t0_end
+            track["t"] = drift_time + track["t0"]
+            track["t_start"] = min(drift_start, drift_end) / detector.V_DRIFT + track["t0"]
+            track["t_end"] = max(drift_start, drift_end) / detector.V_DRIFT + track["t0"]
