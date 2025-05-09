Adjust digit_samples calculation to fix inaccuracy
digit_samples uses the ceil() function to get an integer, however this
can produce an unexpected result if the floating point calculation
produces a value slightly higher than it should assuming infinite
precision.

For example, 0.96+8.64 should equal 9.6 exactly, however it evalulates
to 9.600000000000001 when running. This error can cause ceil() to then
return a higher integer than expected. To complete the example for the
FSD parameters, this results in waveforms with 601 instead of 600 ticks.

This fix uses round() to reduce the calculation of the light_trig_window
size to three decimal places before using ceil() to eliminate the
inaccuracy at very high precision.

The use of round() inside the ceil() function should retain the correct
behavior if the light_trig_window is not an even multiple of the
digit_sample_spacing

cli/simulate_pixels.py
@@ -788,9 +788,8 @@ def run_simulation(input_filename,
 
             light_lut = light_lut_filename[i_mod-1] if mod2mod_variation else light_lut_filename
 
-            if (i_mod == 1
-                or (mod2mod_variation
-                    and light_lut != light_lut_filename[i_mod-2])):
+            if (i_mod == -1 or
+               (mod2mod_variation and (i_mod == 1 or light_lut != light_lut_filename[i_mod-2]))):
 
                 lut = np.load(light_lut)['arr']
 
@@ -844,7 +843,7 @@ def run_simulation(input_filename,
                 op_channel = light.TPC_TO_OP_CHANNEL[:2].ravel() if mod2mod_variation else light.TPC_TO_OP_CHANNEL[:].ravel()
                 op_channel = cp.array(op_channel)
                 trigger_op_channel_idx = cp.repeat(np.expand_dims(op_channel, axis=0), len(trigger_idx), axis=0)
-                digit_samples = ceil((light.LIGHT_TRIG_WINDOW[1] + light.LIGHT_TRIG_WINDOW[0]) / light.LIGHT_DIGIT_SAMPLE_SPACING)
+                digit_samples = ceil(round(light.LIGHT_TRIG_WINDOW[1] + light.LIGHT_TRIG_WINDOW[0], 3) / light.LIGHT_DIGIT_SAMPLE_SPACING)
 
                 n_light_det = op_channel.shape[0]
                 n_light_ticks = int((light.LIGHT_WINDOW[1] + light.LIGHT_WINDOW[0])/light.LIGHT_TICK_SIZE)
@@ -1240,7 +1239,7 @@ def run_simulation(input_filename,
                     light_threshold = light_threshold.ravel()[op_channel.get()].copy()
                     light_threshold = light_threshold.reshape(-1, light.OP_CHANNEL_PER_TRIG)[...,0]
                     trigger_idx, trigger_op_channel_idx, trigger_type = light_sim.get_triggers(light_response, light_threshold, op_channel, itrk)
-                    digit_samples = ceil((light.LIGHT_TRIG_WINDOW[1] + light.LIGHT_TRIG_WINDOW[0]) / light.LIGHT_DIGIT_SAMPLE_SPACING)
+                    digit_samples = ceil(round(light.LIGHT_TRIG_WINDOW[1] + light.LIGHT_TRIG_WINDOW[0], 3) / light.LIGHT_DIGIT_SAMPLE_SPACING)
                     TPB = (1,1,64)
                     BPG = (max(ceil(trigger_idx.shape[0] / TPB[0]),1),
                            max(ceil(trigger_op_channel_idx.shape[1] / TPB[1]),1),