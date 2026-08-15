067367152ee73d7ddf53180fbd821ac35f28719b
Matt Kramer <mjkramer@users.noreply.github.com>
2026-08-15 13:42:42 -0700

    tmp switch to near_field_dWdz

--- larndsim/far_field/signal_calculation.py
+++ larndsim/far_field/signal_calculation.py
@@ -253,9 +253,13 @@ def calculate_ff_segments(
             dy = y - y_pixel
             dz = z - z_anode
 
-            C = ff_induction.DIPOLE_SCALE
+            # C = ff_induction.DIPOLE_SCALE
+            C = 1.
             C *= 0.01 # something something units
-            dWdz = C * dipole_dWdz(dx, dy, dz, l, ff_induction.DIPOLE_N_TERMS)
+            # dWdz = C * dipole_dWdz(dx, dy, dz, l, ff_induction.DIPOLE_N_TERMS)
+            pix_halfw = 0.18
+            dWdz = C * near_field_dWdz(dx, dy, dz, pix_halfw, pix_halfw, l,
+                                       ff_induction.DIPOLE_N_TERMS)
 
             total_current += -q_piece * detector.V_DRIFT * dWdz
 
